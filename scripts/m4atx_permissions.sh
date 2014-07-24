4e0f93a929a0a9c133274ffec89fce8d4053a5db
Russell Toris <rctoris@wpi.edu>
2014-07-24 10:04:57 -0400

    comments

--- scripts/m4atx_permissions.sh
+++ scripts/m4atx_permissions.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-###
-#Start Up script for the M4ATX power supply to ensure proper permissions 
-#when executing the ROS node
-###
-
+# Script for the M4ATX power supply to ensure proper permissions when executing the ROS node
 BUS_NUM=$((lsusb | grep "04d8:d001 Microchip Technology, Inc.") | awk '{print $2}')
 DEV_NUM=$(((lsusb | grep "04d8:d001 Microchip Technology, Inc.") | awk '{print $4}') | cut -c 1-3)
 
